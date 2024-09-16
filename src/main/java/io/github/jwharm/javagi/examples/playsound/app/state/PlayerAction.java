28b7b380891006a1cbd7f07b7e2c20c2a6bb8c14
Eivind Siqveland Larsen <eivind@siqve.land>
2024-09-16 20:40:56 +0200

    fix PlayQueue exploding in logs

--- src/main/java/io/github/jwharm/javagi/examples/playsound/app/state/PlayerAction.java
+++ src/main/java/io/github/jwharm/javagi/examples/playsound/app/state/PlayerAction.java
@@ -11,7 +11,12 @@ public sealed interface PlayerAction {
     record PlayPrev() implements PlayerAction {}
     record PlayNext() implements PlayerAction {}
     record SeekTo(Duration position) implements PlayerAction {}
-    record PlayQueue(List<SongInfo> queue, int position) implements PlayerAction {}
+    record PlayQueue(List<SongInfo> queue, int position) implements PlayerAction {
+        @Override
+        public String toString() {
+            return "PlayQueue([%d songs], position=%d)".formatted(this.queue.size(), position);
+        }
+    }
     record PlayPositionInQueue(int position) implements PlayerAction {}
     record PlaySong(SongInfo song) implements PlayerAction {}
     record Enqueue(SongInfo song) implements PlayerAction {}
