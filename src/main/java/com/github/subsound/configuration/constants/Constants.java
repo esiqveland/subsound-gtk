d7e87e193d55439f94f882eba88da9b9e139c729
Eivind Siqveland Larsen <eivind@siqve.land>
2024-10-02 22:39:05 +0200

    build files

---
+++ src/main/java/com/github/subsound/configuration/constants/Constants.java
@@ -0,0 +1,7 @@
+package com.github.subsound.configuration.constants;
+
+public class Constants {
+    public static final class Application {
+        public static final String VERSION = "@version@";
+    }
+}
