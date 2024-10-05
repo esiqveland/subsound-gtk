4da84a357c2ecc899a5d5d0a96667c105a4dc03c
Eivind Siqveland Larsen <eivind@siqve.land>
2024-10-05 11:14:07 +0200

    icon 24

--- src/main/resources/icons/icons.sh
+++ src/main/resources/icons/icons.sh
@@ -11,4 +11,5 @@ magick convert icon-512.png -strip -resize 128x128 generated/icon-128.png
 magick convert icon-512.png -strip -resize 64x64   generated/icon-64.png
 magick convert icon-512.png -strip -resize 48x48   generated/icon-48.png
 magick convert icon-512.png -strip -resize 32x32   generated/icon-32.png
+magick convert icon-512.png -strip -resize 24x24   generated/icon-24.png
 magick convert icon-512.png -strip -resize 16x16   generated/icon-16.png
