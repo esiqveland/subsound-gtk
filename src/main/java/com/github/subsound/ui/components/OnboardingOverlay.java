a30df675a4399cd4813bc1bda22b8bd1b0f9d4fa
Eivind Siqveland Larsen <eivind@siqve.land>
2024-10-07 20:50:38 +0200

    Update OnboardingOverlay.java

--- src/main/java/com/github/subsound/ui/components/OnboardingOverlay.java
+++ src/main/java/com/github/subsound/ui/components/OnboardingOverlay.java
@@ -3,12 +3,18 @@ package com.github.subsound.ui.components;
 import com.github.subsound.utils.Utils;
 import org.gnome.adw.HeaderBar;
 import org.gnome.adw.ToolbarView;
+import org.gnome.gdk.Paintable;
+import org.gnome.gdk.Texture;
+import org.gnome.gdkpixbuf.Pixbuf;
 import org.gnome.gtk.Align;
 import org.gnome.gtk.Box;
+import org.gnome.gtk.Image;
 import org.gnome.gtk.Justification;
 import org.gnome.gtk.Label;
 import org.gnome.gtk.Orientation;
 import org.gnome.gtk.Overlay;
+import org.gnome.gtk.Picture;
+import org.gnome.gtk.Separator;
 import org.gnome.gtk.Widget;
 
 public class OnboardingOverlay extends Overlay {
@@ -19,6 +25,7 @@ public class OnboardingOverlay extends Overlay {
     private final HeaderBar headerBar;
     private final ToolbarView toolbarView;
     private final Box contentBox;
+    private final Picture logo;
 
     public OnboardingOverlay(SettingsPage settingsPage, Widget child) {
         super();
@@ -30,10 +37,13 @@ public class OnboardingOverlay extends Overlay {
         this.settingsBox.addCssClass(Classes.shadow.className());
         this.settingsBox.addCssClass(Classes.background.className());
         this.centerBox.append(this.settingsBox);
+        this.logo = image(ImageIcons.SubsoundLarge);
+        this.logo.setMarginBottom(20);
         this.headerBar = new HeaderBar();
         this.headerBar.setTitleWidget(Label.builder().setLabel("").setCssClasses(Classes.title1.add()).build());
         this.contentBox = Box.builder().setOrientation(Orientation.VERTICAL).setSpacing(0).build();
-        this.contentBox.append(Label.builder().setLabel("Welcome to Subsound").setJustify(Justification.CENTER).setCssClasses(Classes.titleLarge.add()).setMarginBottom(20).build());
+        this.contentBox.append(Label.builder().setLabel("Welcome").setJustify(Justification.CENTER).setCssClasses(Classes.titleLarge.add()).setMarginBottom(20).build());
+        this.contentBox.append(this.logo);
         this.contentBox.append(Label.builder().setLabel("Login to your Subsonic Server to get started.").setJustify(Justification.CENTER).setCssClasses(Classes.bodyText.add()).setMarginBottom(10).build());
         this.contentBox.append(this.settingsPage);
         this.toolbarView = new ToolbarView();
@@ -45,5 +55,11 @@ public class OnboardingOverlay extends Overlay {
         this.child.addCssClass(Classes.blurred.className());
         this.setChild(child);
         this.setChildVisible(true);
+    }
+
+    private Picture image(ImageIcons imageIcons) {
+        Texture texture = Texture.forPixbuf(imageIcons.getPixbuf());
+        var p = Picture.forPaintable(texture);
+        return p;
     }
 }
