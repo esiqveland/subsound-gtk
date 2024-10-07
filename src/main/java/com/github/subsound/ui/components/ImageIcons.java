d15d811f9c710385daf6af8c715c7032a701478d
Eivind Siqveland Larsen <eivind@siqve.land>
2024-10-07 20:50:26 +0200

    Create ImageIcons.java

---
+++ src/main/java/com/github/subsound/ui/components/ImageIcons.java
@@ -0,0 +1,26 @@
+package com.github.subsound.ui.components;
+
+import com.github.subsound.utils.ImageUtils;
+import org.gnome.gdkpixbuf.Pixbuf;
+
+import java.nio.file.Path;
+
+import static com.github.subsound.utils.Utils.mustReadBytes;
+
+// https://specifications.freedesktop.org/icon-naming-spec/latest/
+// https://gitlab.gnome.org/GNOME/adwaita-icon-theme/-/tree/master/Adwaita/symbolic?ref_type=heads
+public enum ImageIcons {
+    SubsoundLarge(Path.of("/icons/generated/icon-256.png")),
+    SubsoundSmall(Path.of("/icons/generated/icon-64.png")),
+    ;
+
+    private final Pixbuf pixbuf;
+    ImageIcons(Path path) {
+        byte[] bytes = mustReadBytes(path.toString());
+        this.pixbuf = ImageUtils.readPixbuf(bytes);
+    }
+
+    public Pixbuf getPixbuf() {
+        return this.pixbuf;
+    }
+}
