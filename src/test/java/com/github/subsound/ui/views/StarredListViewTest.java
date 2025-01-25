ae31f940fc462b2ee7cba9e497317ac9aadd431b
Eivind Siqveland Larsen <eivind@siqve.land>
2025-01-25 23:01:40 +0100

    Create StarredListViewTest.java

---
+++ src/test/java/com/github/subsound/ui/views/StarredListViewTest.java
@@ -0,0 +1,18 @@
+package com.github.subsound.ui.views;
+
+import org.gnome.gio.ListStore;
+import org.gnome.gobject.GObject;
+import org.junit.Test;
+
+public class StarredListViewTest {
+
+    @Test
+    public void testClear() {
+        var list = new ListStore<>(GObject.getType());
+        // add a single item so its no longer empty:
+        list.add(GObject.builder().build());
+        list.clear(); // throws UnsupportedOperationException
+        //list.removeAll(); // removeAll works
+    }
+
+}
