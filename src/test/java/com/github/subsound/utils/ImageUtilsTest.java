52209a1871c355b7a28b81da993317d1590507e1
Eivind Siqveland Larsen <eivind@siqve.land>
2026-02-22 20:06:17 +0100

    Update ImageUtilsTest.java

--- src/test/java/com/github/subsound/utils/ImageUtilsTest.java
+++ src/test/java/com/github/subsound/utils/ImageUtilsTest.java
@@ -4,6 +4,7 @@ import org.javagi.base.GErrorException;
 import org.javagi.base.Out;
 import org.gnome.gdkpixbuf.Pixbuf;
 import org.gnome.glib.GError;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.imageio.ImageIO;
@@ -40,6 +41,18 @@ public class ImageUtilsTest {
     }
 
     @Test
+    @Ignore
+    // This test crashes in flatpak environment:
+    //
+    // org.javagi.base.GErrorException: Loader process exited early with status '1'Command:
+    // env -i XDG_RUNTIME_DIR="/run/user/1000" "flatpak-spawn" "--sandbox" "--watch-bus" "--directory=/" "--forward-fd=127" "prlimit" "--as=17012097024" "/usr/libexec/glycin-loaders/2+/glycin-image-rs" "--dbus-fd" "127"
+    //	at org.gnome.gdkpixbuf.Pixbuf.constructFromFileAtSize(Pixbuf.java:517)
+    //	at org.gnome.gdkpixbuf.Pixbuf.fromFileAtSize(Pixbuf.java:500)
+    //	at com.github.subsound.utils.ImageUtilsTest.testPixbufToBufferedImage(ImageUtilsTest.java:46)
+    //	at java.base/jdk.internal.reflect.DirectMethodHandleAccessor.invoke(DirectMethodHandleAccessor.java:104)
+    //	at java.base/java.lang.reflect.Method.invoke(Method.java:565)
+    //	at org.junit.runners.model.FrameworkMethod$1.runReflectiveCall(FrameworkMethod.java:59)
+    //	at org.junit.internal.runners.model.ReflectiveCallable.run(ReflectiveCallable.java:12)
     public void testPixbufToBufferedImage() throws GErrorException, IOException {
         var path = Path.of("src/test/resources/fixtures/test2.jpg");
         int size = 300;
