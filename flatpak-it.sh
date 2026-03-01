122acba2ca5f4c82446df9f8af722bbcbd536ff0
Eivind Siqveland Larsen <eivind@siqve.land>
2026-03-01 22:55:57 +0100

    Update flatpak-it.sh

--- flatpak-it.sh
+++ flatpak-it.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+appstreamcli validate src/main/resources/app/com.github.Subsound.metainfo.xml
+
 flatpak-builder --force-clean --user --install-deps-from=flathub --repo=repo --install builddir com.github.Subsound.yml
 
 
