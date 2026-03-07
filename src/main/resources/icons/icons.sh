f248c98c365f305e5b508c67bdb8c9ec3e042f81
Eivind Siqveland Larsen <eivind@siqve.land>
2026-03-07 16:53:14 +0100

    icons

--- src/main/resources/icons/icons.sh
+++ src/main/resources/icons/icons.sh
@@ -5,11 +5,6 @@ set -ex
 rm -rf generated
 mkdir -p generated
 
-magick convert icon-512.png -strip -resize 512x512 generated/icon-512.png
-magick convert icon-512.png -strip -resize 256x256 generated/icon-256.png
-magick convert icon-512.png -strip -resize 128x128 generated/icon-128.png
-magick convert icon-512.png -strip -resize 64x64   generated/icon-64.png
-magick convert icon-512.png -strip -resize 48x48   generated/icon-48.png
-magick convert icon-512.png -strip -resize 32x32   generated/icon-32.png
-magick convert icon-512.png -strip -resize 24x24   generated/icon-24.png
-magick convert icon-512.png -strip -resize 16x16   generated/icon-16.png
+for size in 512 256 128 64 48 32 24 16; do
+    rsvg-convert -w $size -h $size icon.svg -o generated/icon-${size}.png
+done
