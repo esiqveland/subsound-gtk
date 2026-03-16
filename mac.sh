3ff7bedeafe091750761067ddc26c2e3ef642344
Eivind Larsen <to.eivind@gmail.com>
2026-03-16 11:58:09 +0100

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -13,6 +13,7 @@ links=$(pkg-config --cflags gstreamer-1.0 gtk4)
 
 java \
   -XstartOnFirstThread \
+  -Djava.awt.headless=true \
   --enable-native-access=ALL-UNNAMED \
   -Djava.library.path=/usr/lib64:/lib64:/lib:/usr/lib:/lib/x86_64-linux-gnu:/opt/homebrew/lib:/opt/homebrew/lib/gstreamer-1.0 \
   -jar build/libs/*final.jar
