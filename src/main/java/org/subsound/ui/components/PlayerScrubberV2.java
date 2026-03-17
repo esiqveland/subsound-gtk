1efbd2ea1d921626c3de0c8b5fc1e315f46bbb04
Eivind Siqveland Larsen <eivind@siqve.land>
2026-03-17 18:02:56 +0100

    Update PlayerScrubberV2.java

--- src/main/java/org/subsound/ui/components/PlayerScrubberV2.java
+++ src/main/java/org/subsound/ui/components/PlayerScrubberV2.java
@@ -1,18 +1,18 @@
 package org.subsound.ui.components;
 
-import org.gnome.gdk.RGBA;
 import org.gnome.gdk.PaintableFlags;
+import org.gnome.gdk.RGBA;
 import org.gnome.gobject.GObject;
 import org.gnome.graphene.Rect;
-import org.gnome.gsk.RoundedRect;
 import org.gnome.gtk.Align;
 import org.gnome.gtk.Box;
 import org.gnome.gtk.ContentFit;
+import org.gnome.gtk.EventSequenceState;
 import org.gnome.gtk.GestureDrag;
-import org.gnome.gtk.GestureClick;
 import org.gnome.gtk.Label;
 import org.gnome.gtk.Orientation;
 import org.gnome.gtk.Picture;
+import org.gnome.gtk.PropagationPhase;
 import org.subsound.utils.Utils;
 
 import java.lang.foreign.Arena;
@@ -34,21 +34,22 @@ public class PlayerScrubberV2 extends GObject implements org.gnome.gdk.Paintable
             float trackHeight = 3.0f;
             float trackY = (float) ((height - trackHeight) / 2.0);
 
+            float radius = 0.0f;
             // Track background
-            appendRoundedRect(arena, snapshot, 0, trackY, (float) width, trackHeight, 1.0f,
+            appendRoundedRect(arena, snapshot, 0, trackY, (float) width, trackHeight, radius,
                     new RGBA(0.24f, 0.24f, 0.24f, 1.0f));
 
             // Fill / buffer indicator
             if (fill > 0.0) {
                 float fillWidth = (float) (fill * width);
-                appendRoundedRect(arena, snapshot, 0, trackY, fillWidth, trackHeight, 1.0f,
+                appendRoundedRect(arena, snapshot, 0, trackY, fillWidth, trackHeight, radius,
                         new RGBA(0.35f, 0.35f, 0.35f, 1.0f));
             }
 
             // Progress
             float progressWidth = (float) (position * width);
             if (progressWidth > 0) {
-                appendRoundedRect(arena, snapshot, 0, trackY, progressWidth, trackHeight, 1.0f,
+                appendRoundedRect(arena, snapshot, 0, trackY, progressWidth, trackHeight, radius,
                         new RGBA(0.21f, 0.52f, 0.90f, 1.0f));
             }
         }
@@ -65,9 +66,9 @@ public class PlayerScrubberV2 extends GObject implements org.gnome.gdk.Paintable
             RGBA color
     ) {
         var rect = new Rect(arena).init(x, y, w, h);
-        var rr = new RoundedRect(arena);
-        rr.initFromRect(rect, radius);
-        snapshot.pushRoundedClip(rr);
+        //var rr = new RoundedRect(arena);
+        //rr.initFromRect(rect, radius);
+        snapshot.pushClip(rect);
         snapshot.appendColor(color, rect);
         snapshot.pop();
     }
@@ -104,13 +105,15 @@ public class PlayerScrubberV2 extends GObject implements org.gnome.gdk.Paintable
         private double endTimeSecs = 0.0;
 
         private final AtomicBoolean isDragging = new AtomicBoolean(false);
+        private volatile double pictureWidthDrag;
+        private volatile double dragStartX;
         private double dragPosition = 0.0;
-        private double startX = 0.0;
 
         public ScrubberWidget(Consumer<Duration> onPositionChanged) {
             super(Orientation.HORIZONTAL, 5);
             this.onPositionChanged = onPositionChanged;
             this.paintable = new PlayerScrubberV2();
+            //var widgetPaint = new WidgetPaintable();
 
             currentTimeLabel = Label.builder()
                     .setLabel(LABEL_ZERO)
@@ -129,8 +132,9 @@ public class PlayerScrubberV2 extends GObject implements org.gnome.gdk.Paintable
 
             picture = Picture.forPaintable(paintable);
             picture.setContentFit(ContentFit.FILL);
+            picture.setCanShrink(true);
             picture.setValign(Align.CENTER);
-            picture.setSizeRequest(400, 20);
+            picture.setSizeRequest(400, 32);
 
             setupGestures();
 
@@ -140,30 +144,29 @@ public class PlayerScrubberV2 extends GObject implements org.gnome.gdk.Paintable
         }
 
         private void setupGestures() {
-            GestureClick gestureClick = new GestureClick();
-            gestureClick.onPressed((nPress, x, y) -> {
-                isDragging.set(true);
-                dragPosition = clamp(x / picture.getWidth(), 0.0, 1.0);
-                paintable.setPosition(dragPosition);
-                currentTimeLabel.setLabel(Utils.formatDurationShortest(toAbsoluteDuration(dragPosition)));
-            });
-            gestureClick.onReleased((nPress, x, y) -> {
-                if (isDragging.getAndSet(false)) {
-                    var finalDuration = toAbsoluteDuration(dragPosition);
-                    currentTimeLabel.setLabel(Utils.formatDurationShortest(finalDuration));
-                    onPositionChanged.accept(finalDuration);
-                }
-            });
-            picture.addController(gestureClick);
-
+            // GestureDrag handles both clicks (zero-offset drag) and drags.
+            // setState(CLAIMED) in onDragBegin prevents the window manager from
+            // interpreting the drag as a window move request.
             GestureDrag gestureDrag = new GestureDrag();
-            gestureDrag.onDragBegin((x, y) -> {
-                startX = x;
+            gestureDrag.setPropagationPhase(PropagationPhase.CAPTURE);
+            gestureDrag.onDragBegin((startX, startY) -> {
+                this.isDragging.set(true);
+                this.dragStartX = startX;
+                this.pictureWidthDrag = picture.getWidth();
+                gestureDrag.setState(EventSequenceState.CLAIMED);
             });
-            gestureDrag.onDragUpdate((dx, dy) -> {
-                dragPosition = clamp((startX + dx) / picture.getWidth(), 0.0, 1.0);
+            gestureDrag.onDragUpdate((offsetX, offsetY) -> {
+                dragPosition = clamp((dragStartX + offsetX) / this.pictureWidthDrag, 0.0, 1.0);
                 paintable.setPosition(dragPosition);
                 currentTimeLabel.setLabel(Utils.formatDurationShortest(toAbsoluteDuration(dragPosition)));
+            });
+            gestureDrag.onDragEnd((offsetX, offsetY) -> {
+                isDragging.set(false);
+                dragPosition = clamp((dragStartX + offsetX) / this.pictureWidthDrag, 0.0, 1.0);
+                paintable.setPosition(dragPosition);
+                var finalDuration = toAbsoluteDuration(dragPosition);
+                currentTimeLabel.setLabel(Utils.formatDurationShortest(finalDuration));
+                onPositionChanged.accept(finalDuration);
             });
             picture.addController(gestureDrag);
         }
